da3e1fdd4dab0e6bf94234f98165b6dc4da2a9a8
Tarun Dev <devtarunmagadi@gmail.com>
2025-09-29 17:38:01 +0530

    save to master

--- evenodd.py
+++ evenodd.py
@@ -3,3 +3,17 @@ if num%2==0:
    print("the number is Even")
 else:
    print(num,"the number is Odd")
+
+# Check if a number is prime
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
